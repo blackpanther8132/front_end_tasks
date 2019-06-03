9cee7c3f6bb4584603f0935df3124310f078f678
blackpanther8132 <blackpanther8132@gmail.com>
2019-06-03 22:59:51 +0530

    created chips

---
+++ src/app/Modules/MainApp/components/Navigation/Category.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import FaceIcon from '@material-ui/icons/Face';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+import "../componentsStyle/componentsStyle.css";
+import DoneIcon from '@material-ui/icons/Done';
+import red from '@material-ui/core/colors/red';
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    display: 'flex',
+    margin: 10,
+  },
+
+}));
+
+function Chips() {
+  const classes = useStyles();
+
+  function handleDelete() {
+    alert('You clicked the delete icon.');
+  }
+
+  function handleClick() {
+    
+  }
+
+  return (
+    <div className={classes.root}>
+      <Chip
+      style={{ float: 'left'}}
+        avatar={<Avatar tabIndex={-1} className={classes.avatar}>{<DoneIcon/>}</Avatar>}
+        label="Comedy"
+        tabIndex={0}
+        onClick={handleClick}
+        className={classes.chip}  
+      />
+      <Chip
+      tabIndex={-1}
+      style={{ float: 'left'}}
+        avatar={<Avatar tabIndex={-1} className={classes.avatar}>{<DoneIcon/>}</Avatar>}
+        label="Action"
+        onClick={handleClick}
+        className={classes.chip}  
+      />
+      <Chip
+      tabIndex={-1}
+      style={{ float: 'left'}}
+        avatar={<Avatar tabIndex={-1} className={classes.avatar}>{<DoneIcon/>}</Avatar>}
+        label="Horror"
+        onClick={handleClick}
+        className={classes.chip}  
+      />
+      <Chip
+      tabIndex={-1}
+      style={{ float: 'left'}}
+        avatar={<Avatar tabIndex={-1} className={classes.avatar}>{<DoneIcon/>}</Avatar>}
+        label="Thriller"
+        onClick={handleClick}
+        className={classes.chip}  
+      />
+      <Chip
+      tabIndex={-1}
+      style={{ float: 'left'}}
+        avatar={<Avatar tabIndex={-1} className={classes.avatar}>{<DoneIcon/>}</Avatar>}
+        label="Romantic"
+        onClick={handleClick}
+        className={classes.chip}  
+      />
+      <Chip
+      tabIndex={-1}
+      style={{ float: 'left'}}
+        avatar={<Avatar tabIndex={-1} className={classes.avatar}>{<DoneIcon/>}</Avatar>}
+        label="Marati"
+        onClick={handleClick}
+        className={classes.chip}  
+      />
+    </div>  
+  );
+}
+
+export default Chips;
